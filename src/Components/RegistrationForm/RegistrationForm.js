ea0d8632ac20fcfaba3840e8e6f339997e671b14
Ognjen Kecina <ognjenkecina@gmail.com>
2022-06-18 22:08:06 +0200

    Creates style for the first and the last name inputs

--- src/Components/RegistrationForm/RegistrationForm.js
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -6,6 +6,18 @@ function RegistrationForm(props) {
         <div className={"registration-form-container"}>
             <div>
                 <h1>Registration Form</h1>
+                <div className={"first-name-container"}>
+                    <legend>
+                        <label htmlFor="first-name">First Name</label>
+                    </legend>
+                    <input type="text" id={"first-name"} placeholder={"Insert your first name"}/>
+                </div>
+                <div className={"last-name-container"}>
+                    <legend>
+                        <label htmlFor="last-name">Last Name</label>
+                    </legend>
+                    <input type="text" id={"last-name"} placeholder={"Insert your last name"}/>
+                </div>
             </div>
         </div>
     );
