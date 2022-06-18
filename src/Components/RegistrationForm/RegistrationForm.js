34b18d646dc74ef74a78245a41be560dfc742321
Ognjen Kecina <ognjenkecina@gmail.com>
2022-06-18 22:29:30 +0200

    Creates style for the next step button

--- src/Components/RegistrationForm/RegistrationForm.js
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './registration-form.css';
+import fieldsData from '../../sampleData.json';
 
 function RegistrationForm(props) {
     return (
@@ -8,7 +9,7 @@ function RegistrationForm(props) {
                 <h1>Registration Form</h1>
                 <div className={"first-name-container"}>
                     <legend>
-                        <label htmlFor="first-name">First Name</label>
+                        <label htmlFor="first-name">{fieldsData.fields[3].name}</label>
                     </legend>
                     <input type="text" id={"first-name"} placeholder={"Insert your first name"}/>
                 </div>
@@ -17,6 +18,9 @@ function RegistrationForm(props) {
                         <label htmlFor="last-name">Last Name</label>
                     </legend>
                     <input type="text" id={"last-name"} placeholder={"Insert your last name"}/>
+                </div>
+                <div className={"next-step-button-container"}>
+                    <button>Next Step</button>
                 </div>
             </div>
         </div>
