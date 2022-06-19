ddd64d66ced6da26c0c293e5cfc634d640f5e16d
Ognjen Kecina <ognjenkecina@gmail.com>
2022-06-19 15:05:15 +0200

    Creates translation file

--- src/Components/RegistrationForm/RegistrationForm.js
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -2,8 +2,11 @@ import React, {useEffect, useState} from 'react';
 import './registration-form.css';
 import fieldsData from '../../sampleData.json';
 import {Dialog} from "@mui/material";
+import {useTranslation} from "react-i18next";
 
 function RegistrationForm() {
+    const {t, i18n} = useTranslation();
+
     const [firstNameValue, setFirstNameValue] = useState("");
     const [firstNameError, setFirstNameError] = useState("");
     const [isValidFirstName, setIsValidFirstName] = useState(false);
@@ -33,17 +36,21 @@ function RegistrationForm() {
 
     const [changeForm, setChangeForm] = useState(false);
     const [isOpenModal, setIsOpenModal] = useState(false);
+    const [isUKLanguageVisible, setIsUKLanguageVisible] = useState(false);
 
     const validateFirstName = () => {
         if ((firstNameValue.length) === 0) {
-            setFirstNameError("Required!");
+            let lang = t('Required');
+            setFirstNameError(lang);
             setIsValidFirstName(false);
         } else {
             if ((firstNameValue.length) < fieldsData.fields[3].validators[0].parameters.targetLength) {
-                setFirstNameError("First name must have at least 2 characters!");
+                let lang = t('First name must have at least 2 characters!');
+                setFirstNameError(lang);
                 setIsValidFirstName(false);
             } else if ((firstNameValue.length) > fieldsData.fields[3].validators[1].parameters.targetLength) {
-                setFirstNameError("First name can have max 25 characters!");
+                let lang = t('First name can have max 25 characters!');
+                setFirstNameError(lang);
                 setIsValidFirstName(false);
             } else {
                 setFirstNameError("");
@@ -54,14 +61,17 @@ function RegistrationForm() {
 
     const validateLastName = () => {
         if ((lastNameValue.length) === 0) {
-            setLastNameError("Required!");
+            let lang = t('Required');
+            setLastNameError(lang);
             setIsValidLastName(false);
         } else {
             if ((lastNameValue.length) < fieldsData.fields[4].validators[0].parameters.targetLength) {
-                setLastNameError("Last name must have at least 2 characters!");
+                let lang = t('Last name must have at least 2 characters!');
+                setLastNameError(lang);
                 setIsValidLastName(false);
             } else if ((lastNameValue.length) > fieldsData.fields[4].validators[1].parameters.targetLength) {
-                setLastNameError("Last name can have max 25 characters!");
+                let lang = t('Last name can have max 25 characters!');
+                setLastNameError(lang);
                 setIsValidLastName(false);
             } else {
                 setLastNameError("");
@@ -73,14 +83,16 @@ function RegistrationForm() {
     const validateEmail = () => {
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if ((emailValue.length) === 0) {
-            setEmailError("Required!");
+            let lang = t('Required');
+            setEmailError(lang);
             setIsValidEmail(false);
         } else {
             if (regex.test(emailValue)) {
                 setEmailError("");
                 setIsValidEmail(true);
             } else {
-                setEmailError("Wrong type of email!");
+                let lang = t('Wrong type of email!');
+                setEmailError(lang);
                 setIsValidEmail(false);
             }
         }
@@ -89,17 +101,21 @@ function RegistrationForm() {
     const validateUsername = () => {
         const regex = /^[a-z0-9\-\_]+$/;
         if ((usernameValue.length) === 0) {
-            setUsernameError("Required!");
+            let lang = t('Required');
+            setUsernameError(lang);
             setIsValidUsername(false);
         } else {
             if ((usernameValue.length) < fieldsData.fields[2].validators[0].parameters.targetLength) {
-                setUsernameError("Username must have at least 4 characters!");
+                let lang = t('Username must have at least 4 characters!');
+                setUsernameError(lang);
                 setIsValidUsername(false);
             } else if ((usernameValue.length) > fieldsData.fields[2].validators[1].parameters.targetLength) {
-                setUsernameError("Username can have max 20 characters!");
+                let lang = t('Username can have max 20 characters!');
+                setUsernameError(lang);
                 setIsValidUsername(false);
             } else if (!regex.test(usernameValue)) {
-                setUsernameError("Insert characters from a-z and numbers from 0-9 only!");
+                let lang = t('Insert characters from a-z and numbers from 0-9 only!');
+                setUsernameError(lang);
                 setIsValidUsername(false);
             } else {
                 setUsernameError("");
@@ -111,14 +127,17 @@ function RegistrationForm() {
     const validatePassword = () => {
         const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
         if ((passwordValue.length) === 0) {
-            setPasswordError("Required!");
+            let lang = t('Required');
+            setPasswordError(lang);
             setIsValidPassword(false);
         } else {
             if ((passwordValue.length) < fieldsData.fields[8].validators[0].parameters.targetLength) {
-                setPasswordError("Password must have at least 6 characters!");
+                let lang = t('Password must have at least 6 characters!');
+                setPasswordError(lang);
                 setIsValidPassword(false);
             } else if (!regex.test(passwordValue)) {
-                setPasswordError("Insert stronger password!");
+                let lang = t('Insert stronger password!');
+                setPasswordError(lang);
                 setIsValidPassword(false);
             } else {
                 setPasswordError("");
@@ -129,11 +148,13 @@ function RegistrationForm() {
 
     const validatePasswordConfirmation = () => {
         if ((passwordConfirmationValue.length) === 0) {
-            setPasswordConfirmationError("Required!");
+            let lang = t('Required');
+            setPasswordConfirmationError(lang);
             setIsValidPasswordConfirmation(false);
         } else {
             if (passwordConfirmationValue !== passwordValue) {
-                setPasswordConfirmationError("Password doesn't match!");
+                let lang = t("Password doesn't match!");
+                setPasswordConfirmationError(lang);
                 setIsValidPasswordConfirmation(false);
             } else {
                 setPasswordConfirmationError("");
@@ -144,7 +165,8 @@ function RegistrationForm() {
 
     const validateTermsAndConditions = () => {
         if (!isTermsChecked) {
-            setTermsAndConditionsError("Please read our terms and conditions!");
+            let lang = t('Please read our terms and conditions!');
+            setTermsAndConditionsError(lang);
         } else {
             setTermsAndConditionsError("");
         }
@@ -170,6 +192,16 @@ function RegistrationForm() {
         setPasswordConfirmationValue("");
         setIsTermsChecked(false);
         setChangeForm(false);
+    }
+
+    const handleLanguageClick = () => {
+        if (!isUKLanguageVisible) {
+            i18n.changeLanguage('mne');
+            setIsUKLanguageVisible(true);
+        } else {
+            i18n.changeLanguage('en');
+            setIsUKLanguageVisible(false);
+        }
     }
 
     useEffect(() => {
@@ -203,21 +235,33 @@ function RegistrationForm() {
                 open={isOpenModal}
                 onClose={() => handleClose()}
             >
-                <h2>Success!</h2>
+                <h2>{t('Success!')}</h2>
             </Dialog>
             <div className={"registration-form-container"}>
                 <div>
-                    <h1>Registration Form</h1>
+                    <span
+                        onClick={() => {
+                            handleLanguageClick();
+                        }}
+                        className={"flag"}
+                    >
+                        {isUKLanguageVisible ? (
+                            <img src="./images/uk-flag.png" alt=""/>
+                        ) : (
+                            <img src="./images/mne-flag.png" alt=""/>
+                        )}
+                    </span>
+                    <h1>{t('Registration Form')}</h1>
                     {!changeForm ?
                         <>
                             <div className={"first-name-container"}>
                                 <legend>
-                                    <label htmlFor="first-name">{fieldsData.fields[3].name}</label>
+                                    <label htmlFor="first-name">{t('First Name')}</label>
                                 </legend>
                                 <input
                                     type="text"
                                     id={"first-name"}
-                                    placeholder={"Insert your first name"}
+                                    placeholder={t('Insert your first name')}
                                     value={firstNameValue}
                                     onChange={(e) => {
                                         setFirstNameValue(e.target.value);
@@ -229,12 +273,12 @@ function RegistrationForm() {
                             </div>
                             <div className={"last-name-container"}>
                                 <legend>
-                                    <label htmlFor="last-name">Last Name</label>
+                                    <label htmlFor="last-name">{t('Last Name')}</label>
                                 </legend>
                                 <input
                                     type="text"
                                     id={"last-name"}
-                                    placeholder={"Insert your last name"}
+                                    placeholder={t('Insert your last name')}
                                     value={lastNameValue}
                                     onChange={(e) => {
                                         setLastNameValue(e.target.value);
@@ -251,19 +295,19 @@ function RegistrationForm() {
                                         changeNextStepForm();
                                     }}
                                 >
-                                    Next Step
+                                    {t('Next Step')}
                                 </button>
                             </div>
                         </>
                         : <>
                             <div className={"email-container"}>
                                 <legend>
-                                    <label htmlFor="email">{fieldsData.fields[5].name}</label>
+                                    <label htmlFor="email"> {t('E-mail')}</label>
                                 </legend>
                                 <input
                                     type="text"
                                     id={"email"}
-                                    placeholder={"Insert your email"}
+                                    placeholder={t('Insert your email')}
                                     value={emailValue}
                                     onChange={(e) => {
                                         setEmailValue(e.target.value);
@@ -275,12 +319,12 @@ function RegistrationForm() {
                             </div>
                             <div className={"username-container"}>
                                 <legend>
-                                    <label htmlFor="username">{fieldsData.fields[2].name}</label>
+                                    <label htmlFor="username">{t('Username')}</label>
                                 </legend>
                                 <input
                                     type="text"
                                     id={"username"}
-                                    placeholder={"Insert your username"}
+                                    placeholder={t('Insert your username')}
                                     value={usernameValue}
                                     onChange={(e) => {
                                         setUsernameValue(e.target.value);
@@ -292,12 +336,12 @@ function RegistrationForm() {
                             </div>
                             <div className={"password-container"}>
                                 <legend>
-                                    <label htmlFor="password">{fieldsData.fields[8].name}</label>
+                                    <label htmlFor="password">{t('Password')}</label>
                                 </legend>
                                 <input
                                     type="password"
                                     id={"password"}
-                                    placeholder={"Insert your password"}
+                                    placeholder={t('Insert your password')}
                                     value={passwordValue}
                                     onChange={(e) => {
                                         setPasswordValue(e.target.value);
@@ -309,12 +353,12 @@ function RegistrationForm() {
                             </div>
                             <div className={"password-confirmation-container"}>
                                 <legend>
-                                    <label htmlFor="password-confirmation">{fieldsData.fields[9].name}</label>
+                                    <label htmlFor="password-confirmation">{t('Password Confirmation')}</label>
                                 </legend>
                                 <input
                                     type="password"
                                     id={"password-confirmation"}
-                                    placeholder={"Insert your password again"}
+                                    placeholder={t('Insert your password again')}
                                     value={passwordConfirmationValue}
                                     onChange={(e) => {
                                         setPasswordConfirmationValue(e.target.value)
@@ -325,7 +369,8 @@ function RegistrationForm() {
                             </div>
                             <div className={"terms-and-conditions-container"}>
                             <span>
-                                <label htmlFor="terms-and-conditions">You agree with our terms and conditions?</label>
+                                <label
+                                    htmlFor="terms-and-conditions">{t('You agree with our terms and conditions?')}</label>
                             </span>
                                 <input
                                     type="checkbox"
@@ -343,7 +388,7 @@ function RegistrationForm() {
                                         sendData();
                                     }}
                                 >
-                                    Send
+                                    {t('Send')}
                                 </button>
                             </div>
                         </>
---
+++ src/i18n.js
@@ -0,0 +1,85 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+    en: {
+        translation: {
+            "Registration Form": "Registration Form",
+            "First Name": "First Name",
+            "Insert your first name": "Insert your first name",
+            "Required": "Required!",
+            "First name must have at least 2 characters!": "First name must have at least 2 characters!",
+            "First name can have max 25 characters!": "First name can have max 25 characters!",
+            "Last name must have at least 2 characters!": "Last name must have at least 2 characters!",
+            "Last name can have max 25 characters!": "Last name can have max 25 characters!",
+            "Last Name": "Last Name",
+            "Insert your last name": "Insert your last name",
+            "Next Step": "Next Step",
+            "E-mail": "E-mail",
+            "Insert your email": "Insert your email",
+            "Wrong type of email!": "Wrong type of email!",
+            "Username": "Username",
+            "Insert your username": "Insert your username",
+            "Username must have at least 4 characters!": "Username must have at least 4 characters!",
+            "Username can have max 20 characters!": "Username can have max 20 characters!",
+            "Insert characters from a-z and numbers from 0-9 only!": "Insert characters from a-z and numbers from 0-9 only!",
+            "Password": "Password",
+            "Insert your password": "Insert your password",
+            "Password must have at least 6 characters!": "Password must have at least 6 characters!",
+            "Insert stronger password!": "Insert stronger password!",
+            "Password Confirmation": "Password Confirmation",
+            "Insert your password again": "Insert your password again",
+            "Password doesn't match!": "Password doesn't match!",
+            "You agree with our terms and conditions?": "You agree with our terms and conditions?",
+            "Please read our terms and conditions!": "Please read our terms and conditions!",
+            "Send": "Send",
+            "Success!": "Success!"
+        }
+    },
+    mne: {
+        translation: {
+            "Registration Form": "Registraciona Forma",
+            "First Name": "Ime",
+            "Insert your first name": "Unesite Vaše ime",
+            "Required": "Obavezno polje!",
+            "First name must have at least 2 characters!": "Ime može imati minimum 2 slova!",
+            "First name can have max 25 characters!": "Ime može imati maximum 25 slova!",
+            "Last name must have at least 2 characters!": "Prezime može imati minimum 2 slova!",
+            "Last name can have max 25 characters!": "Prezime može imati maximum 25 slova!",
+            "Last Name": "Prezime",
+            "Insert your last name": "Unesite Vaše prezime",
+            "Next Step": "Sledeći korak",
+            "E-mail": "E-mail",
+            "Insert your email": "Unesite Vaš e-mail",
+            "Wrong type of email!": "Pogrešan format e-mail-a!",
+            "Username": "Korisničko ime",
+            "Insert your username": "Unesite Vaše korisničko ime",
+            "Username must have at least 4 characters!": "Korisničko ime može imati minimum 4 slova!",
+            "Username can have max 20 characters!": "Korisničko ime može imati maximum 25 slova!",
+            "Insert characters from a-z and numbers from 0-9 only!": "Unesite slova od a-z i brojeve od 0-9!",
+            "Password": "Lozinka",
+            "Insert your password": "Unesite Vašu lozinku",
+            "Password must have at least 6 characters!": "Lozinka može imati minimum 6 slova!",
+            "Insert stronger password!": "Unesite sigurniju lozinku!",
+            "Password Confirmation": "Potvrdite lozinku",
+            "Insert your password again": "Pokušajte ponovo!",
+            "Password doesn't match!": "Lozinke se ne poklapaju!",
+            "You agree with our terms and conditions?": "Slažete li se sa uslovima korišćenja",
+            "Please read our terms and conditions!": "Molim Vas pročitajte uslove korišćenja!",
+            "Send": "Pošalji",
+            "Success!": "Uspješno poslati podaci!"
+        }
+    }
+};
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: "en",
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
+export default i18n;
